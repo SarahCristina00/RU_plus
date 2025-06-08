359e2f164c3c113211dd2034f6ded809ca34b24a
Sarah Cristina Freitas da Silva <163673649+SarahCristina00@users.noreply.github.com>
2025-06-08 14:40:36 +0000

    cria função para adicionar saldo

--- routes/recargas.js
+++ routes/recargas.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-//exemplo de dados
 let saldos = {
-  "202312345": 25.00, // exemplo de aluno
-  "202398765": 10.50
+  202312345: 25.0,
+  202398765: 10.5,
 };
-
-// GET /recargas/saldo/:matricula
 
 router.get("/saldo/:matricula", (req, res) => {
   const matricula = req.params.matricula;
@@ -15,11 +12,27 @@ router.get("/saldo/:matricula", (req, res) => {
   if (saldos[matricula] !== undefined) {
     res.json({
       matricula: matricula,
-      saldo: saldos[matricula]
+      saldo: saldos[matricula],
     });
   } else {
     res.status(404).json({ erro: "Matrícula não encontrada." });
   }
+});
+
+router.post("/:matricula", (req, res) => {
+  const matricula = req.params.matricula;
+  const { valor } = req.body;
+
+  if (typeof valor !== "number" || valor <= 0) {
+    return res.status(400).json({ erro: "Valor de recarga inválido." });
+  }
+
+  saldos[matricula] += valor;
+
+  res.json({
+    mensagem: `Recarga de R$${valor.toFixed(2)} realizada com sucesso.`,
+    saldoAtual: saldos[matricula],
+  });
 });
 
 module.exports = router;
