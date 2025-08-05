173707125e3e4a85c8fd8f8247662d6aea4d9577
Sarah Cristina Freitas da Silva <163673649+SarahCristina00@users.noreply.github.com>
2025-08-05 01:15:25 +0000

    atualiza server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const app = express();
-const recargaRoutes = require("./routes/recargasRoutes");
+const recargaRoutes = require("./routes/recargaRoutes");
 
 app.use(express.static("public"));
-
 app.use(express.json());
-
 app.use("/recargas", recargaRoutes);
 
-app.get("/", (req, res) => {
-  res.send("RU+ está no ar");
-});
+app.get("/", (req, res) => res.send("RU+ está no ar"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
