68db91b96b3763bdaa91eb6abacf2f9e50dff7b5
Lara Dias <lara.dias@estudante.ufjf.br>
2026-01-10 18:41:19 -0300

    Modifica rotas de recarga para usar instância do controlador

--- routes/rotasRecarga.js
+++ routes/rotasRecarga.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 const ControladorRecarga = require("../controllers/ControladorRecarga");
 
+const controlador = new ControladorRecarga();
+
 // Rotas
 router.get("/saldo/:matricula", (req, res) =>
-  ControladorRecarga.getSaldo(req, res),
+  controlador.getSaldo(req, res),
 );
 
 router.get("/historico/:matricula", (req, res) =>
-  ControladorRecarga.getHistorico(req, res),
+  controlador.getHistorico(req, res),
 );
 
-router.post("/recarga", (req, res) => ControladorRecarga.recarregar(req, res));
+router.post("/recarga", (req, res) => controlador.recarregar(req, res));
 
 module.exports = router;
